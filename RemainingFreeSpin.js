0bf03a0f13bf18e53002825907ebf8967c5d9b14
Shao-Tsu <72166702+shaotsuc@users.noreply.github.com>
2024-04-04 22:04:02 +0300

    Remaining Free Spin
    Minus one, haven't implemented more indicator

---
+++ RemainingFreeSpin.js
@@ -0,0 +1,9 @@
+var numberOfFreeSpins = 50
+
+while (numberOfFreeSpins >= 0) {
+    console.log("Total Free Spins: " + numberOfFreeSpins);
+    console.log("Player hits Spin button");
+    console.log("\n");
+    numberOfFreeSpins--;
+
+}
